429aa9272a2deaa32728816da265c06f1ab8b4c0
isaacGeorge <44020598+isaacGeorge@users.noreply.github.com>
2021-06-13 10:22:11 +0300

    covered basics of vue

---
+++ main.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+    el: "#app",
+    props: [],
+    components: [],
+    data: {
+        title: "Product goes here"
+    },
+    beforeCreated: function() {
+
+    },
+    created: function() {
+
+    },
+    beforeMount: function() {
+
+    },
+    mounted: function() {
+
+    },
+    beforeDestroyed: function() {
+
+    },
+    destroyed: function() {
+
+    },
+})
